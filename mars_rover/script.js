b0c5ac150a3d7febc2f5e300d126abf8f1699f06
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-06-19 23:30:26 +0530

    made changes in mars rover

--- mars_rover/script.js
+++ mars_rover/script.js
@@ -1,10 +1,10 @@
 
 function loadimages() {
-    $("#image-gallery").empty();
     var datevalue = $('#date').val();
     if(!datevalue) {alert('Please input the date!'); return;}
     $.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=sFSGRCh8zc5zlTuxeYpeB7tnBpf8AQ9gKuMcNRgf&earth_date=' + datevalue, function(data){
         if(data.photos.length == 0) { alert('No image available'); return; }
+        $("#image-gallery").empty();
         for(var i = 0; i < data.photos.length; i++)
         {
             console.log(data.photos[i]);
