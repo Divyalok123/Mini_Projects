88158402a09cf8d337d17a1b0165986f6a1298e7
Divyalok Jaiswal <divyjais2001@gmail.com>
2021-01-28 16:11:10 +0530

    Finished Vue Intro

--- Frameworks_Intro/Vue_Intro/vue-intro/src/App.vue
+++ Frameworks_Intro/Vue_Intro/vue-intro/src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
   </div>
   <router-view />
+  <posts-list></posts-list>
 </template>
+
+<script>
+import PostsList from './components/PostsList';
+
+export default {
+  name: "App",
+  components: {
+    PostsList
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ Frameworks_Intro/Vue_Intro/vue-intro/src/components/PostsList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="dog-post">
+    <h2>A cool <strong>DOG</strong> image below!</h2>
+    <h4><strong>Reload</strong> to see a new one :)</h4>
+
+    <img alt="Dog" :src=posts.message class="dog-image">
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "PostsList",
+
+    data() {
+        return {
+            posts: []
+        }
+    },
+
+    mounted() {
+        axios.get('https://dog.ceo/api/breeds/image/random').then((data) => {
+            console.log("data: ", data);
+            console.log("data.data: ", data.data);
+            console.log("data.data.message: ", data.data.message);
+            this.posts = data.data
+        });
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.dog-image {
+    width: 350px;
+    height: 300px;
+    box-shadow: 0 0 2px 2px lightgray;
+    border-radius: 7%;
+}
+</style>
